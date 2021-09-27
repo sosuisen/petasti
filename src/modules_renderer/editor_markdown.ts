2c9e1f1da4ed3b9e50922cce23429e561f1134d1
sosuisen <hidekazu.kubota@gmail.com>
2021-09-27 16:41:30 +0900

    refactor: remove &nbsp; from syntax-tree

--- src/modules_renderer/editor_markdown.ts
+++ src/modules_renderer/editor_markdown.ts
@@ -5,14 +5,10 @@
 
 import {
   commandsCtx,
-  CommandsReady,
-  createCmdKey,
   defaultValueCtx,
   Editor,
   editorViewCtx,
-  MilkdownPlugin,
   rootCtx,
-  schemaCtx,
   serializerCtx,
 } from '@sosuisen/milkdown-core';
 import { prism } from '@sosuisen/milkdown-plugin-prism';
@@ -52,83 +48,6 @@ const marginTop = 3;
 const marginLeft = 7;
 const padding = 2;
 
-function getNodeEndpoints (
-  context: ProseNode,
-  node: ProseNode
-): null | { from: number; to: number } {
-  let offset = 0;
-  console.log(
-    `# getNodeEndpoint, context[${context.type.name}(${context.textContent})], node[${node.type.name}(${node.textContent})]`
-  );
-
-  if (context === node) {
-    console.log(
-      `  -(match)-> ${JSON.stringify({ from: offset, to: offset + node.nodeSize })}`
-    );
-    return { from: offset, to: offset + node.nodeSize };
-  }
-
-  if (node.isBlock) {
-    console.log(
-      `context[${context.type.name}(${context.textContent})] size ${context.content.childCount}`
-    );
-
-    for (let i = 0; i < context.content.childCount; i++) {
-      console.log(`context[${context.type.name}(${context.textContent})] index ${i}`);
-
-      const result = getNodeEndpoints(context.content.child(i), node);
-      if (result) {
-        console.log(
-          `  -> ${JSON.stringify({
-            // from: result.from + offset + (context.type.kind === null ? 0 : 1),
-            // to: result.to + offset + (context.type.kind === null ? 0 : 1),
-            from: result.from + offset + 1, // Add opening tag of context. Its length is one.
-            to: result.to + offset + 1, // Add opening tag of context. Its length is one.
-          })}`
-        );
-
-        return {
-          // from: result.from + offset + (context.type.kind === null ? 0 : 1),
-          // to: result.to + offset + (context.type.kind === null ? 0 : 1),
-          from: result.from + offset + 1,
-          to: result.to + offset + 1,
-        };
-      }
-      offset += context.content.child(i).nodeSize;
-    }
-    return null;
-  }
-  return null;
-}
-
-const findText = (rootDoc: ProseNode, proseNode: ProseNode, txt: string) => {
-  let result: TextSelection[] = [];
-
-  console.log(`### findText ${proseNode.type.name}(${proseNode.textContent})`);
-
-  if (proseNode.isTextblock) {
-    let index = 0;
-    let foundAt;
-    const ep = getNodeEndpoints(rootDoc, proseNode);
-    // if (ep == null) return result;
-    while ((foundAt = proseNode.textContent.slice(index).search(new RegExp(txt))) > -1) {
-      const sel = new TextSelection(
-        rootDoc.resolve(ep!.from + index + foundAt + 1),
-        rootDoc.resolve(ep!.from + index + foundAt + txt.length + 1)
-      );
-      console.log(`Selection: ${sel.from}, ${sel.to}`);
-      result.push(sel);
-      index = index + foundAt + txt.length;
-    }
-  }
-  else {
-    proseNode.content.forEach(
-      (child, i) => (result = result.concat(findText(rootDoc, child, txt)))
-    );
-  }
-  return result;
-};
-
 export class CardEditorMarkdown implements ICardEditor {
   /**
    * Private
@@ -162,24 +81,89 @@ export class CardEditorMarkdown implements ICardEditor {
     return `<img id="${id}" src="${src}" alt="${alt}" width="${width}" height="${height}" />`;
   };
 
-  adjustEditorSizeFromImage2Plugin = async (imgWidth: number, imgHeight: number) => {};
+  adjustEditorSizeFromImage2Plugin = async (imgWidth: number, imgHeight: number) => { };
+
+
+  calcNodePosition(
+    context: ProseNode,
+    node: ProseNode
+  ): null | { from: number; to: number } {
+    let offset = 0;
+    console.log(
+      `# getNodeEndpoint, context[${context.type.name}(${context.textContent})], node[${node.type.name}(${node.textContent})]`
+    );
+
+    if (context === node) {
+      console.log(
+        `  -(match)-> ${JSON.stringify({ from: offset, to: offset + node.nodeSize })}`
+      );
+      return { from: offset, to: offset + node.nodeSize };
+    }
+
+    if (node.isBlock) {
+      console.log(
+        `context[${context.type.name}(${context.textContent})] size ${context.content.childCount}`
+      );
+
+      for (let i = 0; i < context.content.childCount; i++) {
+        console.log(`context[${context.type.name}(${context.textContent})] index ${i}`);
+
+        const result = this.calcNodePosition(context.content.child(i), node);
+        if (result) {
+          console.log(
+            `  -> ${JSON.stringify({
+              // from: result.from + offset + (context.type.kind === null ? 0 : 1),
+              // to: result.to + offset + (context.type.kind === null ? 0 : 1),
+              from: result.from + offset + 1, // Add opening tag of context. Its length is one.
+              to: result.to + offset + 1, // Add opening tag of context. Its length is one.
+            })}`
+          );
+
+          return {
+            // from: result.from + offset + (context.type.kind === null ? 0 : 1),
+            // to: result.to + offset + (context.type.kind === null ? 0 : 1),
+            from: result.from + offset + 1,
+            to: result.to + offset + 1,
+          };
+        }
+        offset += context.content.child(i).nodeSize;
+      }
+      return null;
+    }
+    return null;
+  }
+
+  findText = (rootDoc: ProseNode, proseNode: ProseNode, txt: string) => {
+    let result: TextSelection[] = [];
+
+    console.log(`### findText ${proseNode.type.name}(${proseNode.textContent})`);
+
+    if (proseNode.isTextblock) {
+      let index = 0;
+      let foundAt;
+      const ep = this.calcNodePosition(rootDoc, proseNode);
+      // if (ep == null) return result;
+      while ((foundAt = proseNode.textContent.slice(index).search(new RegExp(txt))) > -1) {
+        const sel = new TextSelection(
+          rootDoc.resolve(ep!.from + index + foundAt),
+          rootDoc.resolve(ep!.from + index + foundAt + txt.length)
+        );
+        console.log(`Selection: ${sel.from}, ${sel.to}`);
+        result.push(sel);
+        index = index + foundAt + txt.length;
+      }
+    }
+    else {
+      proseNode.content.forEach(
+        (child, i) => (result = result.concat(this.findText(rootDoc, child, txt)))
+      );
+    }
+    return result;
+  };
 
   loadUI = async (_cardCssStyle: CardCssStyle): Promise<void> => {
     this._cardCssStyle = _cardCssStyle;
-
     return await Promise.resolve();
-
-    // Change event
-    /*
-        CKEDITOR.instances.editor.on('change', async () => {
-          const data = CKEDITOR.instances.editor.getData();
-          if (cardStore.getState().body._body !== data) {
-            await cardStore.dispatch(cardBodyUpdateCreator(data));
-
-            render(['TitleBar']);
-          }
-        });
-      */
   };
 
   public setData = async (body: string): Promise<void> => {
@@ -211,30 +195,27 @@ export class CardEditorMarkdown implements ICardEditor {
             // console.log('# parent: ' + range?.parent.type.name);
             if (range?.parent.type.name === 'doc') {
               const commandManager = ctx.get(commandsCtx);
-              commandManager.call(WrapInBulletList); // turn to h1 by default
+              commandManager.call(WrapInBulletList); // turn to BulletList
               event.preventDefault();
             }
           });
         }
       });
 
-    // Reset each mark to be headless.
-    // https://github.com/Saul-Mirone/milkdown/discussions/107
-
+    /**
+     * Reset each mark to be headless.
+     * https://github.com/Saul-Mirone/milkdown/discussions/107
+     */
     gfm
       .configure(blockquote, { headless: true })
       .configure(bulletList, {
         headless: true,
-        //        keymap: {
-        // [SupportedKeys.BulletList]: 'Tab',
-        //        },
       })
       .configure(codeFence, { headless: true })
       .configure(doc, { headless: true })
       .configure(hardbreak, {
         headless: true,
         keymap: {
-          // [SupportedKeys.HardBreak]: ['Enter', 'Shift-Enter'],
           [SupportedKeys.HardBreak]: ['Shift-Enter'],
         },
       })
@@ -253,6 +234,9 @@ export class CardEditorMarkdown implements ICardEditor {
       .configure(text, { headless: true })
       .configure(taskListItem, { headless: true });
 
+    /**
+     * Create editor
+     */
     this._editor = await Editor.make()
       .config(ctx => {
         ctx.set(rootCtx, document.querySelector('#editor'));
@@ -269,18 +253,18 @@ export class CardEditorMarkdown implements ICardEditor {
       //      .use(slash)
       .create();
 
+    /**
+     * Replace paragraph(&nbsp) with paragraph
+     */
     this._editor.action(ctx => {
       const editorView = ctx.get(editorViewCtx);
-      const selections = findText(editorView.state.doc, editorView.state.doc, '\u00a0'); // Finde &nbsp;
-      // let selections = findText(editorView.state.doc, editorView.state.doc, 'a'); // Finde &nbsp;
+      const selections = this.findText(editorView.state.doc, editorView.state.doc, '\u00a0'); // Finde &nbsp;
       console.log('# Search result: ' + JSON.stringify(selections));
-      const transforms = [];
       let selection: TextSelection | undefined;
-
       let offset = 0;
       while ((selection = selections.shift())) {
-        const from = selection.from + offset - 1;
-        const to = selection.to + offset - 1; // 1??
+        const from = selection.from + offset;
+        const to = selection.to + offset;
         const newState = editorView.state.apply(
           editorView.state.tr.deleteRange(from, to)
           // editorView.state.tr.insertText('x', selection.from, selection.to)
@@ -288,107 +272,13 @@ export class CardEditorMarkdown implements ICardEditor {
         console.log(`# transformed: (${from}, ${to}) ` + newState.doc.toString());
         editorView.updateState(newState);
 
-        // delete a chanacter
+        // delete a character
         offset--;
-
-        // transforms.push(transform);
-        // selections = selections.map(s => s.map(editorView.state.doc, transform.maps[0]))
       }
     });
-
-    /*
-    // eslint-disable-next-line complexity
-    this._editor.action(ctx => {
-      const editorView = ctx.get(editorViewCtx);
-
-      // Remove UI of code-fence and set language to <pre>
-      const codeFences = editorView.dom.getElementsByClassName('code-fence');
-      if (codeFences && codeFences.length > 0) {
-        for (let i = 0; i < codeFences.length; i++) {
-          const codeFenceElm = codeFences[i] as HTMLDivElement;
-          const ui = codeFenceElm.getElementsByClassName('code-fence_select-wrapper');
-          if (ui && ui.length > 0) {
-            const uiElm = ui[0];
-            if (uiElm) codeFenceElm.removeChild(uiElm);
-          }
-          const pre = codeFenceElm.getElementsByTagName('pre');
-          if (pre && pre.length > 0) {
-            const preElm = pre[0];
-            preElm.dataset.language = codeFenceElm.dataset.language;
-          }
-        }
-      }
-
-      // Remove icon and placeholder in image
-      const images = editorView.dom.getElementsByClassName('image');
-      if (images && images.length > 0) {
-        for (let i = 0; i < images.length; i++) {
-          const imageElm = images[i] as HTMLDivElement;
-          const icon = imageElm.getElementsByClassName('icon');
-          if (icon && icon.length > 0) {
-            const iconElm = icon[0];
-            if (iconElm) imageElm.removeChild(iconElm);
-          }
-          const placeholder = imageElm.getElementsByClassName('placeholder');
-          if (placeholder && placeholder.length > 0) {
-            const placeholderElm = placeholder[0];
-            if (placeholderElm) imageElm.removeChild(placeholderElm);
-          }
-        }
-      }
-
-      // Remove label from TaskListItem
-      const taskListItems = editorView.dom.getElementsByClassName('task-list-item');
-      if (taskListItems && taskListItems.length > 0) {
-        for (let i = 0; i < taskListItems.length; i++) {
-          const taskListItemElm = taskListItems[i] as HTMLDivElement;
-          const div = taskListItemElm.getElementsByTagName('div');
-          if (div && div.length > 0) {
-            const divElm = div[0];
-            divElm.childNodes.forEach(child => {
-              taskListItemElm.appendChild(child);
-            });
-            taskListItemElm.removeChild(divElm);
-          }
-          const label = taskListItemElm.getElementsByTagName('label');
-          if (label && label.length > 0) {
-            const labelElm = label[0];
-            if (labelElm) taskListItemElm.removeChild(labelElm);
-          }
-        }
-      }
-
-      const innerHTML = editorView.dom.innerHTML;
-      const fixedHTML = innerHTML.replace(
-        /<p class="paragraph">&nbsp;<\/p>/g,
-        '<p class="paragraph"></p>'
-      );
-      console.log('inner: ' + innerHTML);
-      console.log('fixed: ' + fixedHTML);
-      editorView.dom.innerHTML = fixedHTML;
-      // editorView.dom.innerHTML = innerHTML;
-    });
-    */
-    /*
-    this._editor.action(ctx => {
-      const editorView = ctx.get(editorViewCtx);
-      const parser = ctx.get(parserCtx);
-      console.log('setData: ' + body);
-      const md = parser(body);
-      console.log('#parsed : ' + md!.content!.toString());
-      if (!md) return;
-      const tr = editorView.state.tr;
-      // setData replaces existing text
-      const newState = editorView.state.apply(
-        // tr.insertText(md, 0, editorView.state.doc.content.size)
-        tr.replaceSelectionWith(md)
-      );
-      editorView.updateState(newState);
-    });
-    */
   };
 
-  private _addDragAndDropEvent = () => {};
+  private _addDragAndDropEvent = () => { };
 
   showEditor = (): void => {
     if (this.isOpened) {
